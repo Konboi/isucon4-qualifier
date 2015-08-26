324e6cf6f6c6599d8094b44bf5774b7335d692ef
Konboi <ryosuke.yabuki@gmail.com>
2015-08-26 19:54:43 +0900

    初期化コマンドを追加

--- init.sh
+++ init.sh
@@ -7,7 +7,12 @@ myuser=root
 mydb=isu4_qualifier
 myhost=127.0.0.1
 myport=3306
+
 mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}; CREATE DATABASE ${mydb}"
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/schema.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_users.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
+
+# ベンチ前にredisの初期化とキャッシュを載せる
+echo "FLUSHALL" | redis-cli
+cd /home/isucon/webapp/ruby && bundle exec ruby init_data.rb
